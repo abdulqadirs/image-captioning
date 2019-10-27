73f0412f8dc98033737573a82e5eae9a0a4ff2ef
Abdul Qadir <aqadir.bese15seecs@seecs.edu.pk>
2019-10-27 18:26:14 +0500

    added BLEU-1, BLEU-2, BLEU-3 and BLEU-4

--- captioning/metrics/bleu.py
+++ captioning/metrics/bleu.py
@@ -9,12 +9,20 @@ def bleu_score(reference_caption, predicted_caption):
         predicted_caption (tensor): the caption predicted by the model
 
     Returns:
-        bleu (float): bleu score(0-1)
+        bleu1, bleu2, bleu3, bleu4 (float): bleu score(0-1)
     """
 
     reference_caption = reference_caption.squeeze(0)
     reference_caption = [caption.item() for caption in reference_caption]
     sf = SmoothingFunction()
-    bleu = sentence_bleu([reference_caption], predicted_caption, smoothing_function=sf.method2)
+    bleu1 = sentence_bleu([reference_caption], predicted_caption, weights = [1],
+                            smoothing_function = sf.method2)
+    bleu2 = sentence_bleu([reference_caption], predicted_caption, weights = [0.5, 0.5],
+                            smoothing_function = sf.method2)
+    bleu3 = sentence_bleu([reference_caption], predicted_caption, weights = [0.33, 0.33, 0.33], 
+                            smoothing_function = sf.method2)
+    bleu4 = sentence_bleu([reference_caption], predicted_caption, weights = [0.25, 0.25, 0.25, 0.25], 
+                            smoothing_function = sf.method2 )
+    #bleu = sentence_bleu([reference_caption], predicted_caption, smoothing_function=sf.method2)
 
-    return bleu
+    return bleu1, bleu2, bleu3, bleu4
